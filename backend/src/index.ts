102cd4050c00062dfc8d52c2e913182ab3fc398b
iago-abner <iagoabner.dev@gmail.com>
2024-04-27 18:04:21 -0300

    feat: dockerfile backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
-import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-app.use(routes);
+app.get("/", (_, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
