75846d0260278e32c17435fb89e47ed51eb317ff
iago-abner <iagoabner.dev@gmail.com>
2024-04-28 00:19:57 -0300

    fix: dockerfile to backend and db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-app.get("/", (_, res) => {
-  res.send("Hello World!");
+app.get("health-check", (_, res) => {
+  res.send("OK");
 });
+
+app.use(routes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
